89aefe5ae67142cb8db1a656e009a64e4ca23e31
Kostiantyn <kostiaolnev@gmail.com>
2026-07-06 18:19:14 -0700

    feat: delete LoginRequest class

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel, Field, EmailStr
 
 
-class LoginRequest(BaseModel):
-    username: str = Field(min_length=3, max_length=50)
-    email: EmailStr = Field(max_length=255)
-    password: str = Field(min_length=8, max_length=255)
-
-
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
